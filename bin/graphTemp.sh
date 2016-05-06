fee2166ab8d1c6b659dc2045bd580a87fd731279
Manuel <Manuel@localhost>
2016-05-06 18:46:46 +0200

    Added first graphing script

---
+++ bin/graphTemp.sh
@@ -0,0 +1,25 @@
+export TZ="CET"
+export LC_ALL="de_DE.UTF-8"
+BASEDIR=/opt/klima
+GRAPHDIR=$BASEDIR/graphics
+DATADIR=$BASEDIR/data
+FILENAME=$GRAPHDIR/temp$1.png
+
+rrdtool graph $FILENAME -w 800 -h 400 -D -l 0 -u 30 -s "-$1" \
+-P \
+--title "Temperatur" \
+--vertical-label "T [°C]" \
+--right-axis-label "T [°C]" \
+--right-axis 1.0:0 \
+DEF:Taussen=$DATADIR/aussen.rrd:T:AVERAGE \
+DEF:Tkeller=$DATADIR/keller.rrd:T:AVERAGE \
+DEF:DPaussen=$DATADIR/aussen.rrd:DP:AVERAGE \
+DEF:DPkeller=$DATADIR/keller.rrd:DP:AVERAGE \
+DEF:on=$DATADIR/fan.rrd:on:AVERAGE \
+CDEF:Fan=on,.9,GT,Tkeller,0,IF \
+AREA:Fan#00ff0080: \
+LINE2:Taussen#000000:Aussen \
+LINE2:Tkeller#ff0000:Keller \
+LINE2:DPaussen#0000ff:"Taupunkt aussen" \
+LINE2:DPkeller#ff00ff:"Taupunkt Keller" \
+TICK:Fan#00ff0080:0.0:"Lüfter an"
